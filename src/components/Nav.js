14ee897de14512c8a59310d2853062bb701b4d4f
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-05-30 17:11:26 +0530

    Little-Lemon Restaurant Website

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../assets/icons_assets/Logo.svg'
+
+const Nav = () => {
+  return (
+    <>
+    <nav>
+        <ul>
+            <li><img src={logo} alt="Logo"/></li>
+            <li></li>
+            <li></li>
+            <li></li>
+
+        </ul>
+       
+    </nav>
+    </>
+  )
+}
+
+export default Nav
